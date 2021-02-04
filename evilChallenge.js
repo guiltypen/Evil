683fce0821bbb605c5d81c51345c0e8b5ddf9985
Bader Al-Ramadan <bader.ram@gmail.com>
2021-02-04 19:42:10 +0300

    where is your ears Joha?

--- evilChallenge.js
+++ evilChallenge.js
@@ -1,4 +1,4 @@
-let floors = 4; //how many floors
+let floors = 6; //how many floors
 
 buildTower(floors); //function call
 
@@ -30,4 +30,39 @@ e.g.
   '***********'
 ]
 */
-function buildTower(nFloors) {}
+function buildTower(nFloors) {
+  let i = 1;
+  let floor = 1;
+  let space = [""];
+  let star = ["*"];
+  let count = nFloors;
+  let x = 0;
+  let spaceString = [];
+  let starString = ["*"];
+  while (i <= nFloors) {
+    while (count != 1) {
+      space[x + 1] = space[0];
+      count--;
+      x++;
+      //console.log(space);
+      // console.log(count);
+    }
+    x = 0;
+    let countStars = floor;
+    while (countStars != 0) {
+      star[x] = star[0];
+      countStars--;
+      x++;
+      //console.log(space);
+      // console.log(count);
+    }
+    floor++;
+    count = nFloors - i;
+    let spaceString = space.join(" ");
+    let starString = star.join(" ");
+    console.log(`'${spaceString + starString + spaceString}'`);
+    space = [""];
+    x = 0;
+    i++;
+  }
+}
